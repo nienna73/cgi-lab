b8dbd8eaf4cb08ac8eeea7ec7e8b9f7ab92588e1
jcpoulin <jcpoulin@ualberta.ca>
2020-01-20 18:10:49 -0700

    Resolved cookie issues and local python version issues

--- login.py
+++ login.py
@@ -4,25 +4,41 @@ import cgi
 import cgitb
 from templates import login_page, secret_page, after_login_incorrect
 import secret
+from http.cookies import SimpleCookie
+import os
 cgitb.enable()
 
 s = cgi.FieldStorage()
 username = s.getfirst("username")
 password = s.getfirst("password")
 
-print("Content-Type: text/html\n")
+print("Content-type: text/html")
 form_ok = username==secret.username and password==secret.password
-
 if form_ok:
-    print("Set-Cookie: username=", username)
-    print("Set-Cookie: password=", password)
+    print("Set-cookie:username=" + username)
+    print("Set-cookie:password=" + password)
+
+c = SimpleCookie(os.environ["HTTP_COOKIE"])
+c_username = None
+c_password = None
+if c.get("username"):
+    c_username = c.get("username").value
+if c.get("password"):
+    c_password = c.get("password").value
+
+cookie_ok = c_username == secret.username and c_password == secret.password
+if cookie_ok:
+    username = c_username
+    password = c_password
+
+
 
 print()
 
 if not username and not password:
     print(login_page())
 elif username == secret.username and password == secret.password:
-    print(secret_page())
+    print(secret_page(username, password))
 else:
     print(after_login_incorrect())
 
